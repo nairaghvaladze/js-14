40b00903539e853f321cce1ede61801bbfb33b0e
nairaghvaladze <120264867+nairaghvaladze@users.noreply.github.com>
2023-05-15 00:12:24 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function show() {
+    document.getElementById("circle1").style.right="-1200px";
+    document.getElementById("circle1").style.backgroundColor="green";
+    document.getElementById("circle1").style.animationDuration="5s";
+
+     
+
+
+}
